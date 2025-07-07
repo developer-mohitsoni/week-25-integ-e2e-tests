ad2f1c49e0d9f76ed027a0ab180ca812027c50eb
Mohit Soni <developer_mohitsoni@outlook.com>
2025-07-07 19:35:39 +0530

    add integration tests for summation of positive and negative numbers and run test before each test

--- 1-integration-test/src/tests/sum.test.ts
+++ 1-integration-test/src/tests/sum.test.ts
@@ -1,8 +1,13 @@
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 import { app } from "..";
 import request from "supertest";
+import { clearDb } from "./helpers/reset-db.js";
 
 describe("POST /sum", () => {
+    beforeEach(async () => {
+        console.log("clearing db");
+        await clearDb();
+    });
     it("should sum add 2 numbers", async () => {
         const { status, body } = await request(app).post('/sum').send({
             a: 1,
@@ -10,5 +15,14 @@ describe("POST /sum", () => {
         })
         expect(status).toBe(200);
         expect(body).toEqual({ answer: 3, id: expect.any(Number) });
+    });
+
+    it("should sum add 2 negative numbers", async () => {
+        const { status, body } = await request(app).post('/sum').send({
+            a: -1,
+            b: -2
+        })
+        expect(status).toBe(200);
+        expect(body).toEqual({ answer: -3, id: expect.any(Number) });
     });
 })
